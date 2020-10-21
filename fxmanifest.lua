acfe3f6bad101b60615bbe51fb1a5d758c3ffcd2
Dawson Godin <33760026+DawsonGodin@users.noreply.github.com>
2020-10-21 16:26:27 -0700

    Add game to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,5 @@
 fx_version 'bodacious'
+game 'gta5'
 
 author 'CitizenFX | Dawson Godin (Modified)'
 description 'It\'s a basic resource for making screenshots of clients\' game render targets using FiveM. It uses the same backing WebGL/OpenGL ES calls as used by the application/x-cfx-game-view plugin (see the code in citizenfx/fivem), and wraps these calls using Three.js to \'simplify\' WebGL initialization and copying to a buffer from asynchronous NUI.'
